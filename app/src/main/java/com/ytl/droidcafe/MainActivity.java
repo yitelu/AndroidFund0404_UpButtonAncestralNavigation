a9141d34f4610049ca3be4a9bb173278ff3bd66f
Richard Yi-Te Lu <yitelu@gmail.com>
2018-12-12 22:16:39 -0800

    AppBar Overflow Menu Options Snackbar

--- app/src/main/java/com/ytl/droidcafe/MainActivity.java
+++ app/src/main/java/com/ytl/droidcafe/MainActivity.java
@@ -50,13 +50,25 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()){
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+                Intent intent = new Intent(getApplicationContext(), OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
